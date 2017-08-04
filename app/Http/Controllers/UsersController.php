c30a715f595e952d11ded6b6d4cc709dca4e56c4
Enes <kul.enes@gmail.com>
2017-08-04 15:48:49 +0200

    check

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -23,11 +23,40 @@ class UsersController extends Controller
         return response()->json($subscriptions);
     }
 
-    public function checkSubscription(Request $request) {
-        $data = $request->all();
+    public function checkSubscription(Request $request)
 
-        $subscription = SubscriptionUser::where('user_id', $data['user_id'])->get();
-        return response()->json($subscription);
+    {
+        $data = $request->all();
+        $user = User::where('token', $data['token'])->first();
+        if (empty($user)):
+            return response()->json(['error' => 'Subscription failed']);
+        endif;
+        $subscription = SubscriptionUser::where('user_id', $user->id)->get();
+        if ($subscription->isNotEmpty()):
+            $now = date('Y-m-d');
+            $data = $subscription->first();
+            $endsAt = date("Y-m-d", $data->ends_at);
+            $ended = $this->dateDifference($now, $endsAt);
+            if ($ended > 0):
+                return response()->json(['true']);
+            else:
+                $data->delete();
+                return response()->json(['false']);
+            endif;
+        else:
+            return response()->json(['false']);
+        endif;
+    }
+
+    function dateDifference($date_1, $date_2, $differenceFormat = '%r%a')
+    {
+        $datetime1 = date_create($date_1);
+        $datetime2 = date_create($date_2);
+
+        $interval = date_diff($datetime1, $datetime2);
+
+        return (int)$interval->format($differenceFormat);
+
     }
 
     public function subscribe(Request $request)
@@ -52,14 +81,15 @@ class UsersController extends Controller
         endif;
     }
 
-    public function unSubscribe(Request $request) {
+    public function unSubscribe(Request $request)
+    {
         $data = $request->all();
         $user = User::where('token', $data['token'])->first();
 
         if (empty($user)):
             return response()->json(['error' => 'Subscription failed']);
         endif;
-            
+
         $subscription = SubscriptionUser::where('subscription_id', $data['subscription_id'])->get()->first();
         $subscription->delete();
 
